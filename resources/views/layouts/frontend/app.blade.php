f3fd9e33ff91812249089b2fe4fb932eed1a30af
nizar-sys <raspbery132@gmail.com>
2022-09-17 22:38:30 +0700

    edit real path assets

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -16,7 +16,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('/frontend/css/homepage.css') }}">
-    <link rel="stylesheet" href="{{ asset('/frontend/css/loading.cs') }}s">
+    <link rel="stylesheet" href="{{ asset('/frontend/css/loading.css') }}">
 
 
 
